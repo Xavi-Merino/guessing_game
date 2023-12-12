12856b8889523b6039e504736e586c5f5ccd1891
Xavi-Merino <jamerino@alumnos.uai.cl>
2023-12-12 02:56:46 -0300

    Implemented a number guessing game in main.rs. The game generates a random number between 1 and 100 and asks the user to guess it. The user's input is read from the standard input and compared to the secret number. The game provides feedback whether the guessed number is too small, too big, or correct. If the guess is correct, the user wins and the game ends.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
-use std::io;
+use std::{io, cmp::Ordering};
+use rand::Rng;
 
 fn main (){
-    printl
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    
+    loop{
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 =  match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Win");
+                break;
+            }
+
+        }
+    }
 }
